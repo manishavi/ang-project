a8a0e97e8237416a9b7f91cc13b5c2ecb890a58e
manishavi <manishalal972@gmail.com>
2018-06-29 12:19:30 -0500

    created a list of hero

---
+++ my-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Batman' },
+  { id: 2, name: 'Superman' },
+  { id: 3, name: 'Heman' },
+  { id: 4, name: 'Flash' },
+  { id: 5, name: 'Spiderman' },
+  { id: 6, name: 'Wonder Woman' },
+  { id: 7, name: 'Captain America' },
+  { id: 8, name: 'Black Panthar' },
+  { id: 9, name: 'Thor' },
+  { id: 10, name: 'Wolvorine' },
+];
