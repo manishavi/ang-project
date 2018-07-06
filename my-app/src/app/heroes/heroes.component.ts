7e6ef3eeb9da5656b263510940cd16db03384a9e
manishavi <manishalal972@gmail.com>
2018-07-05 22:46:29 -0500

    add getHeroes and call in ngOnInit

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -3,22 +3,26 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
