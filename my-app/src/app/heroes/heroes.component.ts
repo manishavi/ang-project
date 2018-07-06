0c5d9fda46f13b3f83ca54758df31d207043af56
manishavi <manishalal972@gmail.com>
2018-07-05 22:54:52 -0500

    typo

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -3,9 +3,9 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"]
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
@@ -18,11 +18,11 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
-  getHeroes(): void {
+getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
   }
 }
